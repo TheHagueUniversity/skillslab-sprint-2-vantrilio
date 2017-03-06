e95bffc56495a941e62a210068ca9a7db5a99404
vantrilio <siwek.2006@gmail.com>
2017-03-06 12:46:25 +0100

    6 march comitted for SURE

--- digital skills/js/js.js
+++ digital skills/js/js.js
@@ -2,3 +2,10 @@ function adjust_textarea(h) {
     h.style.height = "20px";
     h.style.height = (h.scrollHeight)+"px";
 }
+function enableScrollwheel(map) {
+    if(map) map.setOptions({ scrollwheel: true });
+}
+
+function disableScrollwheel(map) {
+    if(map) map.setOptions({ scrollwheel: false });
+}
